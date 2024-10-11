f7f7c6baeab24939a04dee0fad198a7dc638a847
sreyesg <santiagoreyes.g@gmail.com>
2024-10-11 13:56:37 -0700

    Resolve Challenge: 02-addTwoNumbers

--- challenges.js
+++ challenges.js
@@ -62,9 +62,15 @@ addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
 
+function addTwoNumbers(num1, num2){
 
-
-
+  if(isNaN(num1) || isNaN(num2)){
+    return 'NaN'
+  }
+  else {
+    return num1 + num2
+  }
+}
 
 /*-----------------------------------------------------------------------------
 Challenge: 03-sumNumbers
